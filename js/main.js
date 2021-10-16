1a5af8f0c8b09f2137b2d3ee48e427be74968ca6
7Liberal7 <89695467+7Liberal7@users.noreply.github.com>
2021-10-16 19:19:38 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,150 @@
+$(function(){
+    $('.header__language__menu__img').on('click', function(){
+		$('.menu__list__language').toggleClass('menu__list__language--active')
+	});
+    $('.menu2__btn__img1').on('click', function on(){
+        $('.assortment__list').addClass('assortment__list--active')
+        document.querySelector('.assortment__list--active').style.background = '#7FAD39'
+        $('.assortment__item-link1').addClass('assortment__item-link1--active')
+        document.querySelector('.assortment__item-link1--active').style.color = '#fff';
+        $('.assortment__item-link2').addClass('assortment__item-link2--active')
+        document.querySelector('.assortment__item-link2--active').style.color = '#fff';
+        $('.assortment__item-link3').addClass('assortment__item-link3--active')
+        document.querySelector('.assortment__item-link3--active').style.color = '#fff';
+        $('.assortment__item-link4').addClass('assortment__item-link4--active')
+        document.querySelector('.assortment__item-link4--active').style.color = '#fff';
+        $('.assortment__item-link5').addClass('assortment__item-link5--active')
+        document.querySelector('.assortment__item-link5--active').style.color = '#fff';
+        $('.assortment__item-link5').addClass('assortment__item-link5--active')
+        document.querySelector('.assortment__item-link3--active').style.color = '#fff';
+        $('.assortment__item-link6').addClass('assortment__item-link6--active')
+        document.querySelector('.assortment__item-link6--active').style.color = '#fff';
+        $('.assortment__item-link8').addClass('assortment__item-link8--active')
+        document.querySelector('.assortment__item-link8--active').style.color = '#fff';
+        $('.assortment__item-link7').addClass('assortment__item-link7--active')
+        document.querySelector('.assortment__item-link7--active').style.color = '#fff';
+        $('.assortment__item-link9').addClass('assortment__item-link9--active')
+        document.querySelector('.assortment__item-link9--active').style.color = '#fff';
+        $('.assortment__item-link10').addClass('assortment__item-link10--active')
+        document.querySelector('.assortment__item-link10--active').style.color = '#fff';
+        $('.assortment__item-link10').addClass('assortment__item-link10--active')
+        document.querySelector('.assortment__item-link10--active').style.color = '#fff';
+        $('.assortment__item-link11').addClass('assortment__item-link11--active')
+        document.querySelector('.assortment__item-link11--active').style.color = '#fff';
+        setTimeout(off, 500) 
+	});
+    $('.menu2__btn__img2').on('click', function on(){
+        $('.assortment__list').addClass('assortment__list--active')
+        document.querySelector('.assortment__list--active').style.background = '#7FAD39';
+        $('.assortment__item-link1').addClass('assortment__item-link1--active')
+        document.querySelector('.assortment__item-link1--active').style.color = '#fff';
+        $('.assortment__item-link2').addClass('assortment__item-link2--active')
+        document.querySelector('.assortment__item-link2--active').style.color = '#fff';
+        $('.assortment__item-link3').addClass('assortment__item-link3--active')
+        document.querySelector('.assortment__item-link3--active').style.color = '#fff';
+        $('.assortment__item-link4').addClass('assortment__item-link4--active')
+        document.querySelector('.assortment__item-link4--active').style.color = '#fff';
+        $('.assortment__item-link5').addClass('assortment__item-link5--active')
+        document.querySelector('.assortment__item-link5--active').style.color = '#fff';
+        $('.assortment__item-link5').addClass('assortment__item-link5--active')
+        document.querySelector('.assortment__item-link3--active').style.color = '#fff';
+        $('.assortment__item-link6').addClass('assortment__item-link6--active')
+        document.querySelector('.assortment__item-link6--active').style.color = '#fff';
+        $('.assortment__item-link8').addClass('assortment__item-link8--active')
+        document.querySelector('.assortment__item-link8--active').style.color = '#fff';
+        $('.assortment__item-link7').addClass('assortment__item-link7--active')
+        document.querySelector('.assortment__item-link7--active').style.color = '#fff';
+        $('.assortment__item-link9').addClass('assortment__item-link9--active')
+        document.querySelector('.assortment__item-link9--active').style.color = '#fff';
+        $('.assortment__item-link10').addClass('assortment__item-link10--active')
+        document.querySelector('.assortment__item-link10--active').style.color = '#fff';
+        $('.assortment__item-link10').addClass('assortment__item-link10--active')
+        document.querySelector('.assortment__item-link10--active').style.color = '#fff';
+        $('.assortment__item-link11').addClass('assortment__item-link11--active')
+        document.querySelector('.assortment__item-link11--active').style.color = '#fff';
+        setTimeout(off, 500)
+	});
+    function off() {
+        document.querySelector('.assortment__list--active').style.background = '#fff';
+        document.querySelector('.assortment__item-link1--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link2--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link3--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link4--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link5--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link6--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link7--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link8--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link9--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link10--active').style.color = '#999999';
+        document.querySelector('.assortment__item-link11--active').style.color = '#999999';
+    };
+    $('.sections__item__img__close1').on('click', function on(){
+        $('.sections__item__1').addClass('sections__item__1--active')
+        document.querySelector('.sections__item__1--active').style.display = 'none';
+    });
+    $('.sections__item__img__close2').on('click', function on(){
+        $('.sections__item__2').addClass('sections__item__2--active')
+        document.querySelector('.sections__item__2--active').style.display = 'none';
+    });
+    $('.sections__item__img__close3').on('click', function on(){
+        $('.sections__item__3').addClass('sections__item__3--active')
+        document.querySelector('.sections__item__3--active').style.display = 'none';
+    });
+    $('.sections__item__img__close4').on('click', function on(){
+        $('.sections__item__4').addClass('sections__item__4--active')
+        document.querySelector('.sections__item__4--active').style.display = 'none';
+    });
+    $('.sections__item__img1__1').on('click', function on(){
+        document.querySelector('.sections__item__img1__1').style.display = 'none';
+        document.querySelector('.sections__item__img1__1--active').style.display = 'inline-block';
+    });
+    $('.sections__item__img1__1--active').on('click', function on(){
+        document.querySelector('.sections__item__img1__1').style.display = 'inline-block';
+        document.querySelector('.sections__item__img1__1--active').style.display = 'none';
+    });
+    $('.sections__item__img1__2').on('click', function on(){
+        document.querySelector('.sections__item__img1__2').style.display = 'none';
+        document.querySelector('.sections__item__img1__2--active').style.display = 'inline-block';
+    });
+    $('.sections__item__img1__2--active').on('click', function on(){
+        document.querySelector('.sections__item__img1__2').style.display = 'inline-block';
+        document.querySelector('.sections__item__img1__2--active').style.display = 'none';
+    });
+    $('.sections__item__img1__3').on('click', function on(){
+        document.querySelector('.sections__item__img1__3').style.display = 'none';
+        document.querySelector('.sections__item__img1__3--active').style.display = 'inline-block';
+    });
+    $('.sections__item__img1__3--active').on('click', function on(){
+        document.querySelector('.sections__item__img1__3').style.display = 'inline-block';
+        document.querySelector('.sections__item__img1__3--active').style.display = 'none';
+    });
+    $('.sections__item__img1__4').on('click', function on(){
+        document.querySelector('.sections__item__img1__4').style.display = 'none';
+        document.querySelector('.sections__item__img1__4--active').style.display = 'inline-block';
+    });
+    $('.sections__item__img1__4--active').on('click', function on(){
+        document.querySelector('.sections__item__img1__4').style.display = 'inline-block';
+        document.querySelector('.sections__item__img1__4--active').style.display = 'none';
+    });
+    $('.sections__item__img2__1').on('click', function on(){
+        alert("Добавить в корзину набор 'FRESH FRUIT' в корзину?");
+    });
+    $('.sections__item__img2__2').on('click', function on(){
+        alert("Добавить в корзину набор 'DRIED FRUIT' в корзину?");
+    });
+    $('.sections__item__img2__3').on('click', function on(){
+        alert("Добавить в корзину набор 'VEGETABLES' в корзину?");
+    });
+    $('.sections__item__img2__4').on('click', function on(){
+        alert("Добавить в корзину набор 'DRINK FRUITS' в корзину?");
+    });
+    $('.sections__item__img2__4').on('click', function on(){
+        alert("Добавить в корзину набор 'DRINK FRUITS' в корзину?");
+    });
+    $('.menu__list__language-item1').on('click', function on(){
+        alert("change language to russian ?");
+    });
+    $('.menu__list__language-item2').on('click', function on(){
+        alert("сменить язык на английский ?");
+    });
+});
